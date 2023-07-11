d00e6c90e062d22dd0b2074454bfb5e93f64e4b9
put your ghofranjdaradkh <Ghofran_Daradkh@hotmail.com>
2023-07-11 08:45:57 +0300

    class-16

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,17 +5,22 @@ import db from '../../data/db.json';
 
 
 
-function Home (){
-    return (
-        <>
-        <Header/>
-        {db.map((obj, i) => (
-            <div className="div"> <Tours key={i} name={obj.name} image={obj.image}/></div>)
-       )}
-        <Footer/>
+function Home() {
 
+  return (
+    <>
+      <Header />
 
-        </>
-    )
+      <Tours  data={db} />
+
+      <Footer />
+
+    </>)
 }
+
+
+
+
+
+
 export default Home
--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -1,11 +1,51 @@
 
 import './Tours.css'
-function Tours(props){
-    return (
-        <div className='tour'>
-        <h1> {props.name}</h1>
-        <img src={props.image}  style={{ width: '500px', height: '500px' }} />
+import db from '../../data/db.json';
+
+
+
+function Tours(props) {
+  return (
+    <div>
+      {db.map((obj, i) => (
+        <div key={i} className="div">
+          <h3>{obj.name}</h3>
+          <img src={obj.image} alt={obj.alt} style={{ width: '500px', height: '500px' }}/>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   return (
+  //     <div>
+  //       {db.map((obj, i) => (
+  //         <div className="div" >
+  //           <Home  key={i} name={obj.name} image={obj.image} />
+  //         </div>
+  //       ))}
+  //     </div>
+  //   );
+  // }
+        
+    //     <div className='tour'>
+    //     <h1> {props.name}</h1>
+    //     <img src={props.image}  style={{ width: '500px', height: '500px' }} />
+    //     </div>
+    // )
+
 export default Tours
